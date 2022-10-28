b1823e4bb6a6f6383b4ecb2e8b7eb0998370be5c
David Lukens <davidqcy02@gmail.com>
2022-10-28 10:07:06 -0500

    added full factorial analysis using method of least squares

--- main.R
+++ main.R
@@ -1,4 +1,10 @@
-x <- 16
-y <- 5
-z <- x + y
-print(z)
+df <- read.delim("data.txt")
+
+full_factorial <- aov(data = df, Time ~ Rider +
+  Board +
+  Backpack +
+  Rider:Board +
+  Rider:Backpack +
+  Board:Backpack)
+summary(full_factorial)
+anova_full <- anova(full_factorial)
