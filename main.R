3bb6cce3edc941689a99a53fa87024f2426341b8
David Lukens <davidqcy02@gmail.com>
2022-10-28 10:11:30 -0500

    add a refined model

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 df <- read.delim("data.txt")
 
+# fit the entire model
 full_factorial <- aov(data = df, Time ~ Rider +
   Board +
   Backpack +
@@ -8,3 +9,11 @@ full_factorial <- aov(data = df, Time ~ Rider +
   Board:Backpack)
 summary(full_factorial)
 anova_full <- anova(full_factorial)
+
+# remove insignificant interactions
+refined <- aov(data = df, Time ~ Rider +
+  Board +
+  Backpack +
+  Board:Backpack)
+summary(refined)
+anova_refined <- anova(full_factorial)
